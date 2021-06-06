89e3704a244bd80431935ec9b1dbb28910e2fa80
Alyssa <alymaebenipayo@gmail.com>
2021-06-05 23:02:04 -0700

    Added top to scroll button.

--- _site/assets/js/guidepages.js
+++ _site/assets/js/guidepages.js
@@ -76,24 +76,25 @@ function scrollHandler() {
 };
 
 /***************************************************/
-/**** Script 3: Accordion *****/
+/**** Script 3: Scroll to the Top (Mobile) *****/
 /***************************************************/
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     /* Toggle between adding and removing the "active" class,
-//     to highlight the button that controls the panel */
-//     this.classList.toggle("active");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-//     /* Toggle between hiding and showing the active panel */
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
